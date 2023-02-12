65e70894d469d9fca053ad413361a3b68d6735ab
iberis2 <snow9402@gmail.com>
2023-02-12 16:26:04 +0900

    isLoding 되는 동안 더보기 diabled 되도록 구현

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ function App() {
   const [order, setOrder] = useState("createdAt");
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -20,6 +21,16 @@ function App() {
   };
 
   const handleLoad = async (options) => {
+    let result;
+    try {
+      setIsLoading(true);
+      result = await getReviews(options);
+    } catch (error) {
+      console.error(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
     const { reviews, paging } = await getReviews(options);
     if (options.offset === 0) {
       setItems(reviews);
@@ -45,7 +56,11 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      {hasNext && <button onClick={handleLoadMore}>더 보기</button>}
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
     </div>
   );
 }
