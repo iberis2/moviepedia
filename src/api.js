9c64605625a24a90bd7da093598320e00d2c808a
iberis2 <snow9402@gmail.com>
2023-02-12 16:37:05 +0900

    에러 발생시, 에러 메세지 출력

--- src/api.js
+++ src/api.js
@@ -5,8 +5,11 @@ export async function getReviews({
 }) {
   const query = `order=${order}&offset=${offset}&limit=${limit}`;
   const response = await fetch(
-    `https://learn.codeit.kr/2221/film-reviews?${query}`
+    `https://learn.codeit.kr/1001/film-reviews?${query}`
   );
+  if (!response.ok) {
+    throw new Error("리뷰를 불러오지 못했습니다.");
+  }
   const body = await response.json();
   return body;
 }
