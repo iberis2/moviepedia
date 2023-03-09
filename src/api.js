0259391dc3f081725d2cc68aa243760ccc938873
iberis2 <snow9402@gmail.com>
2023-03-09 21:40:19 +0900

    이미지 파일 추가 기능

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ export async function getReviews({
 }) {
   const query = `order=${order}&offset=${offset}&limit=${limit}`;
   const response = await fetch(
-    `https://learn.codeit.kr/1001/film-reviews?${query}`
+    `https://learn.codeit.kr/3101/film-reviews?${query}`
   );
   if (!response.ok) {
     throw new Error("리뷰를 불러오지 못했습니다.");
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import ReviewList from "./ReviewList";
 import { useState, useEffect } from "react";
 import { getReviews } from "../api";
+import ReviewForm from "./ReviewForm";
 
 const LIMIT = 6;
 
@@ -57,6 +58,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
         <button onClick={handleNewestClick}>최신순</button>
       </div>
+      <ReviewForm />
       <ReviewList items={sortedItems} onDelete={handleDelete} />
       {hasNext && (
         <button disabled={isLoading} onClick={handleLoadMore}>
