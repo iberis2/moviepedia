a172011b579eddf9a9651fd32b6f3d91132b7d2a
iberis2 <snow9402@gmail.com>
2023-04-14 23:15:13 +0900

    feat: 삭제 API 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import ReviewList from "./ReviewList";
 import { useState, useEffect } from "react";
-import { createReviews, getReviews, updateReview } from "../api";
+import { createReviews, deleteReview, getReviews, updateReview } from "../api";
 import ReviewForm from "./ReviewForm";
 
 const LIMIT = 6;
@@ -17,9 +17,10 @@ function App() {
 
   const handleNewestClick = () => setOrder("createdAt");
   const handleBestClick = () => setOrder("rating");
-  const handleDelete = (id) => {
-    const nextItems = items.filter((item) => item.id !== id);
-    setItems(nextItems);
+  const handleDelete = async (id) => {
+    const result = await deleteReview(id);
+    if (!result) return;
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
   const handleLoad = async (options) => {
